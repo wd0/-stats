a837f91856aeb39f49bd9c6e9fbfe2691c22518e
wd0 <q1000.p0001@gmail.com>
2017-04-11 14:51:53 -0400

    Update pooling.hs

--- pooling.hs
+++ pooling.hs
@@ -6,7 +6,12 @@ printLn :: Show a => a -> IO ()
 printLn x = putStrLn $ show x
     
 getPooled asuc an bsuc bn = (asuc + bsuc)/(an + bn)
-getPooledSE p an bn = sqrt $ p*(1 - p)/an + p*(1-p)/bn
+getPooledSE asuc an bsuc bn = sqrt $ p*(1 - p)/an + p*(1-p)/bn
+  where p = getPooled asuc an bsuc bn
+getTestStat asuc an bsuc bn = (ap - bp)/pooledSE
+  where ap = asuc / an
+        bp = bsuc / bn
+        pooledSE = getPooledSE asuc an bsuc bn 
 
 getSE as an bs bn = sqrt $ as^2/an + bs^2/bn
 getPropSE ap an bp bn = sqrt $ ap*(1-ap)/an + bp*(1-bp)/bn
@@ -25,10 +30,14 @@ getDF as an bs bn = numer/denom
         denom = 1/(an - 1)*arat + 1/(bn - 1)*brat 
 
 main = do
-  let asuc = 174
-      an = 1057
-      bsuc = 184
-      bn = 890
-      p = rnd 4 $ getPooled asuc an bsuc bn
-  printLn $ rnd 4 $ getPooledSE p an bn 
-  printLn $ getConfInterval 0.95 (getPropSE 0.857 12636 0.842 12894) 0.857 0.842
+  let asuc = 47
+      an = 146
+      bsuc = 5 
+      bn = 76
+      ap = asuc/an
+      bp = bsuc/bn
+      se = getPropSE ap an bp bn
+      pool = getPooled asuc an bsuc bn
+      poolSE = getPooledSE pool an bn
+  printLn $ getConfInterval 0.95 se bp ap
+  printLn $ getTestStat asuc an bsuc bn
