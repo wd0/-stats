08878339039bdcb245098c2564a088a65d20068d
wd0 <q1000.p0001@gmail.com>
2017-04-18 11:26:45 -0400

    Update correl.hs

--- correl.hs
+++ correl.hs
@@ -19,10 +19,20 @@ covar xs ys = (/(n - 1)) . sum $ zipWith (*) [x - xm | x <- xs] [y - ym | y <- y
 correl xs ys = (/ (sdx * sdy)) $ covar xs ys 
   where sdx = stddev xs
         sdy = stddev ys
+        
+linreg xs ys = 
+  \x -> b0 + b1 * x
+  where r = correl xs ys
+        b1 = r * (stddev ys)/(stddev xs)
+        b0 = (mean ys) - b1*(mean xs)
 
 main = do
-  let xs = [310, 285, 205, 170, 125, 140, 115, 127]
-      ys = [10, 13, 20, 18, 31, 23, 29, 29]
-
-  -- -0.936
-  put $ correl xs ys
+  let xs = [20,31,33,35,38,38,44]
+      ys = [430,580,570,550,660,690,650]
+      r = correl xs ys
+  put $ r^2
+  put $ (linreg xs ys) 20
+  let b1 = r * (stddev ys)/(stddev xs)
+  let b0 = (mean ys) - b1*(mean xs)
+  put $ b1
+  put $ b0
